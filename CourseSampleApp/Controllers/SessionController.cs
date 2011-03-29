a5df3187589084d7bcae6d240c680538fc773c79
Ayende Rahien <ayende@ayende.com>
2011-03-29 14:33:08 +0100

    Adding a session controller, allowing us to access the NHibernate session

---
+++ CourseSampleApp/Controllers/SessionController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using NHibernate;
+
+namespace CourseSampleApp.Controllers
+{
+	public class SessionController : Controller
+	{
+		public HttpSessionStateBase HttpSession
+		{
+			get { return base.Session;  }
+		}
+
+		public new ISession Session
+		{
+			get { return MvcApplication.CurrentSession; }
+		}
+	}
+}
