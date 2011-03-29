1d34c828952c6be4fa2b30e8b4821d6fd0bb8845
Ayende Rahien <ayende@ayende.com>
2011-03-29 14:12:48 +0100

    Creating initial project Imported NHibernate.Castle & NHhibernateProfiler nuget packages

---
+++ packages/NHibernateProfiler.1.0.0.820/tools/Install.ps1
@@ -0,0 +1,2 @@
+﻿param($installPath, $toolsPath, $package, $project)
+&"$toolsPath\NHProf.exe"
---
+++ packages/NHibernateProfiler.1.0.0.820/tools/Uninstall.ps1
@@ -0,0 +1,2 @@
+﻿param($installPath, $toolsPath, $package, $project)
+Remove-Item "$toolsPath\log.txt" -Force -ErrorAction SilentlyContinue 
